10744d9e69ae64593e6c536200098f895bb16f42
Mads Rasmussen <madshr2005@gmail.com>
2025-10-27 12:27:51 +0100

    chore: Update ESLint configuration and add new plugins for improved linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,65 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import eslintPluginNext from '@next/eslint-plugin-next';
+import eslintPluginJsxA11y from 'eslint-plugin-jsx-a11y';
+import pluginReactHooks from 'eslint-plugin-react-hooks';
+import typescriptEslint from 'typescript-eslint';
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+const eslintIgnore = [
+  '.git/',
+  '.next/',
+  'node_modules/',
+  'dist/',
+  'build/',
+  'coverage/',
+  'playwright-report/',
+  '.npm-only-allow',
+  'test-results',
+  '*.min.js',
+  "next-env.d.ts"
+];
 
-export default eslintConfig;
+const config = typescriptEslint.config(
+  {
+    ignores: eslintIgnore,
+  },
+
+  typescriptEslint.configs.recommended,
+
+  {
+    plugins: {
+      '@next/next': eslintPluginNext,
+      'jsx-a11y': eslintPluginJsxA11y,
+      'react-hooks': pluginReactHooks,
+    },
+
+    rules: {
+      ...eslintPluginNext.configs.recommended.rules,
+      ...eslintPluginNext.configs['core-web-vitals'].rules,
+
+      ...eslintPluginJsxA11y.configs.recommended.rules,
+
+      ...pluginReactHooks.configs.recommended.rules,
+    },
+  },
+  {
+    settings: {
+      react: {
+        version: 'detect',
+      },
+      tailwindcss: {
+        callees: ['classnames', 'clsx', 'ctl', 'cn', 'cva'],
+      },
+      'import/resolver': {
+        typescript: true,
+        node: true,
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['warn'],
+
+      'react/prefer-destructuring-assignment': 'off',
+      'node/prefer-global/process': 'off',
+    },
+  },
+);
+
+export default config;
