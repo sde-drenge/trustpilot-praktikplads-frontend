2c44505a32d3b30ef02223281470e5fab0cba2f2
Ikhvans Mac <ikhvan95@gmail.com>
2025-10-28 14:19:27 +0100

    refactor: Code structure for improved readability and maintainability

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -5,11 +5,11 @@ import { Bell } from 'lucide-react'
 
 export default function Header() {
   return (
-    <header className='flex items-center justify-between p-2.75 bg-gray-950 text-white '>  
+    <header className='flex fixed w-full top-0 left-0 z-50 items-center justify-between p-2.75 bg-gray-950 text-white '>  
 
-      <div className="pl-36 gap-2 pb-1.5 cursor-pointer">
-        <span className="text-4xl font-bold text-[#00B67A]">★</span>
-        <span className="text-2xl font-semibold text-white">Trustpilot</span>
+      <div className="pl-36 gap-2 cursor-pointer">
+        <span className="text-3xl font-bold text-[#00B67A]">★</span>
+        <span className="text-3xl font-semibold text-white">TP</span>
       </div>
 
      <nav className="flex gap-8 font-medium items-center pr-34">
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,51 @@
+'use client'
+
+import { Input } from '@/components/ui/input'
+import Image from 'next/image'
 import React from 'react'
 
 export default function page() {
   return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
-    </div>
+<div className='h-screen pt-17.5 flex flex-col'>
+<section className="relative bg-center bg-no-repeat h-[500px]">
+  <Image
+    src="/images/hero-bg.jpeg"
+    alt="Hero background"
+    fill
+    className="object-cover -z-10"
+    priority
+  />
+
+  <div className="mx-auto max-w-5xl px-8 py-24 text-center">
+    <h1 className="mx-auto max-w-3xl text-4xl font-extrabold tracking-tight text-gray-900 leading-tight">
+      Find en virksomhed, du kan stole
+    </h1>
+  </div>
+
+  <p className="text-center text-black text-lg">
+    Find læs og skriv anmeldelseser
+  </p>
+
+  <div className="mt-8 flex justify-center">
+    <form
+      onSubmit={(e) => e.preventDefault()}
+      className="relative w-full max-w-3xl"
+      role="search"
+    >
+      <Input
+        type="search"
+        placeholder="Søg efter en virksomhed eller kategori"
+        className="w-full rounded-full border border-gray-200 px-5 py-4 mb-10 text-lg"
+      />
+    </form>
+  </div>
+</section>
+
+<section className='flex-1'>
+  </section>
+</div>
+
+
+
   )
 }
