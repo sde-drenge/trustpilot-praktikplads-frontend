1cd130dd3e5945b2bc94eaf8e156365ed9a7a9cb
Ikhvans Mac <ikhvan95@gmail.com>
2025-10-30 12:26:53 +0100

    style: format authentication pages
    - Apply Prettier formatting to login and signup pages

--- app/log-ind/page.tsx
+++ app/log-ind/page.tsx
@@ -1,13 +1,9 @@
 import React from 'react'
 
-
 export default function page() {
   return (
     <div>
-
-      <div className='mt-20'>
-        Signup Page
-      </div>
+      <div className="mt-20">Signup Page</div>
     </div>
   )
 }
